a05596d2fe715855c5c4ce5e3538df2e823675d7
spencercjh <spencercjh@gmail.com>
2024-03-11 20:36:01 +0800

    fix: upgrade to go 1.20; replace logrus by chaosblade-spec-go/util/log
    Signed-off-by: spencercjh <spencercjh@gmail.com>

--- module/errorreturned.go
+++ module/errorreturned.go
@@ -20,11 +20,10 @@ import (
 	"context"
 	"path"
 
-	"github.com/chaosblade-io/chaosblade-spec-go/channel"
-	"github.com/chaosblade-io/chaosblade-spec-go/spec"
-	"github.com/sirupsen/logrus"
-
 	"github.com/chaosblade-io/chaosblade-exec-cplus/common"
+	"github.com/chaosblade-io/chaosblade-spec-go/channel"
+	"github.com/chaosblade-io/chaosblade-spec-go/log"
+	"github.com/chaosblade-io/chaosblade-spec-go/spec"
 )
 
 type ErrorReturnedActionSpec struct {
@@ -87,9 +86,9 @@ func (e *ErrorReturnedExecutor) Exec(uid string, ctx context.Context, model *spe
 	processCtx := context.WithValue(context.Background(), channel.ExcludeProcessKey, "blade")
 	pids, err := channel.NewLocalChannel().GetPidsByProcessName(processName, processCtx)
 	if err != nil {
-		logrus.Warnf("get pids by %s process name err, %v", processName, err)
+		log.Warnf(ctx, "get pids by %s process name err, %v", processName, err)
 	}
-	localChannel := common.NewAsyncChannel()
+	localChannel := channel.NewLocalChannel()
 	if pids == nil || len(pids) == 0 {
 		args := buildArgs([]string{
 			model.ActionFlags["fileLocateAndName"],
--- module/variablemodified.go
+++ module/variablemodified.go
@@ -20,11 +20,10 @@ import (
 	"context"
 	"path"
 
-	"github.com/chaosblade-io/chaosblade-spec-go/channel"
-	"github.com/chaosblade-io/chaosblade-spec-go/spec"
-	"github.com/sirupsen/logrus"
-
 	"github.com/chaosblade-io/chaosblade-exec-cplus/common"
+	"github.com/chaosblade-io/chaosblade-spec-go/channel"
+	"github.com/chaosblade-io/chaosblade-spec-go/log"
+	"github.com/chaosblade-io/chaosblade-spec-go/spec"
 )
 
 type VariableModifiedActionSpec struct {
@@ -93,9 +92,9 @@ func (v *VariableModifiedExecutor) Exec(uid string, ctx context.Context, model *
 	processCtx := context.WithValue(context.Background(), channel.ExcludeProcessKey, "blade")
 	pids, err := channel.NewLocalChannel().GetPidsByProcessName(processName, processCtx)
 	if err != nil {
-		logrus.Warnf("get pids by %s process name err, %v", processName, err)
+		log.Warnf(ctx, "get pids by %s process name err, %v", processName, err)
 	}
-	localChannel := common.NewAsyncChannel()
+	localChannel := channel.NewLocalChannel()
 	if pids == nil || len(pids) == 0 {
 		args := buildArgs([]string{
 			model.ActionFlags["fileLocateAndName"],
